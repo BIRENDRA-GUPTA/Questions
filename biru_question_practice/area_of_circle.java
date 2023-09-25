19291a7d55169d301851a98acafa67b3c262d394
BIRENDRA-GUPTA <rkumar288215@gmail.com>
2023-09-25 17:50:36 +0530

    circle

---
+++ biru_question_practice/area_of_circle.java
@@ -0,0 +1,13 @@
+public class area_of_circle{
+    public static void main(String[] args) {
+        // area of circle
+        int r = 5;
+        System.out.println("area of circle " + 3.14*r*r);
+
+        // perimeter of circle
+        System.out.println("perimeter of circle " + 2*3.14*r);
+
+        // area of rectangle
+        System.out.println("area of rectangle " + r*r);
+    }
+}
